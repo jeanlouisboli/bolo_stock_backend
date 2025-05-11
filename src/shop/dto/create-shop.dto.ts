0968d29f248fce67168bf1ed3a3b2c80f5bdb2fe
Jean-Louis BOLI <jeanlouisboli.developer@gmail.com>
2025-05-11 01:06:43 +0000

    add username

--- src/shop/dto/create-shop.dto.ts
+++ src/shop/dto/create-shop.dto.ts
@@ -4,9 +4,20 @@ import { isFloat32Array } from "util/types"
 
 export class CreateShopDto {
 
+
     @IsString({message:"Le nom de l'entreprise doit etre de chaine de caratere"})
     @IsNotEmpty({ message: 'Le nom est obligatoire' })
     name: string
+
+
+    @IsString({message:"L'username de l'entreprise doit etre de chaine de caratere"})
+    @IsNotEmpty({ message: "L'username est obligatoire" })
+    username: string
+
+
+    @IsString({message:"Le mot de passe de l'entreprise doit etre de chaine de caratere"})
+    @IsNotEmpty({ message: 'Le mot de passe est obligatoire' })
+    password: string
 
 
     @IsEmail({},{message:"Le format de l'email n'est pas respecter"})
