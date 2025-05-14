ab0f94288ba2d009665634aca12d8cd9f2f205d8
Jean-Louis BOLI <jeanlouisboli.developer@gmail.com>
2025-05-14 00:28:38 +0000

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  const config = new DocumentBuilder()
+    .setTitle('Supermarché API')
+    .setDescription('Documentation de l’API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   console.log('🚀 App démarre...');
